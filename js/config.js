16663763df75161ac2c703f2f5b1f1cea15c32b3
caliph21 <73025742+caliph21@users.noreply.github.com>
2025-06-27 12:03:05 +0800

    更新 config.js

--- js/config.js
+++ js/config.js
@@ -22,9 +22,18 @@ const SITE_CONFIG = {
 
 // API站点配置
 const API_SITES = {
+    dyttzy: {
+        api: 'http://caiji.dyttzyapi.com/api.php/provide/vod',
+        name: '电影天堂资源',
+        detail: 'http://caiji.dyttzyapi.com', 
+    },
+    ruyi: {
+        api: 'https://cj.rycjapi.com/api.php/provide/vod',
+        name: '如意资源',
+    },
     qihuziyu: {
         api: 'https://caiji.qhzyapi.com/api.php/provide/vod',
-        name: '奇虎资源'
+        name: '奇虎资源',
     },
     suboziyu: {
         api: 'https://subocaiji.com/api.php/provide/vod',
